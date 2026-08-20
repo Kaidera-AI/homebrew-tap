fae96578189177e36f8bdfadb532182ac585e5f1
Amad Malik <amad@adaptech.ai>
2026-08-20 11:19:49 +0400

    openkai 0.1.8 — re-sha for signed release assets

--- Formula/openkai.rb
+++ Formula/openkai.rb
@@ -7,22 +7,22 @@ class Openkai < Formula
   on_macos do
     on_arm do
       url "https://github.com/Kaidera-AI/OpenKai/releases/download/v0.1.008/openkai-darwin-arm64"
-      sha256 "b6573045fed3d5e0b47544bc3c7536719fa0755049165b092ad6483f004675dc"
+      sha256 "e0da9a6395a1fb265fca52722c8513777ed12c3207c4f025ab6adf87f1ad3839"
     end
     on_intel do
       url "https://github.com/Kaidera-AI/OpenKai/releases/download/v0.1.008/openkai-darwin-x64"
-      sha256 "7a2a0eba6c80ee4e62c2ef9a0eb5930524ec0bdc6e06c6f9f4c8a27582060b07"
+      sha256 "8d8d6dfae13a2bfa5edc22d9a976c7ce5884d0ccc178da04132d63c8e0569c4f"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/Kaidera-AI/OpenKai/releases/download/v0.1.008/openkai-linux-arm64"
-      sha256 "c7161aeb136c42fdc935c2562b99f2aca571abb74ebe1996388a8a9750facb26"
+      sha256 "14d9967bf242caa9d3a90c925a4b73f808fa50858e28984bc3d18c125b6e2517"
     end
     on_intel do
       url "https://github.com/Kaidera-AI/OpenKai/releases/download/v0.1.008/openkai-linux-x64"
-      sha256 "072166988ce103519154ed303b8ed62e9492a4bdc010ec0250461bc6e6e667aa"
+      sha256 "1ab5d5a55725d7a3e585a35027f99b0ac1de7f005257b20f58498d9508a9a062"
     end
   end
 
